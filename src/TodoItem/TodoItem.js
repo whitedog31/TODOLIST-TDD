8b49d0e3d4cf55c04a3f0b30b421a3b7165220e8
김민수 <jasonkim@mondrian.ai>
2022-06-09 14:34:15 +0900

    feat: add readme, todoitem

--- src/TodoItem/TodoItem.js
+++ src/TodoItem/TodoItem.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-export default function TodoItem({ todo }) {
+export default function TodoItem({ todo, onToggle, onRemove }) {
   const { id, text, done } = todo;
-
+  const toggle = useCallback(() => onToggle(id), [id, onToggle]);
+  const remove = useCallback(() => onRemove(id), [id, onRemove]);
   return (
     <li>
-      <span>{text}</span>
-      <button>삭제</button>
+      <span
+        style={{
+          textDecoration: done ? "line-through" : "none",
+        }}
+        onClick={toggle}
+      >
+        {text}
+      </span>
+      <button onClick={remove}>삭제</button>
     </li>
   );
 }
--- src/TodoItem/TodoItem.test.js
+++ src/TodoItem/TodoItem.test.js
@@ -27,13 +27,13 @@ describe("<TodoItem/>", () => {
     expect(button).toBeTruthy();
   });
 
-  // it("shows line-through on span when done is true", () => {
-  //   const { span } = setup({ todo: { ...sampleTodo, done: true } });
-  //   expect(span).toHaveStyle("text-decoration: line-through;");
-  // });
+  it("shows line-through on span when done is true", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: true } });
+    expect(span).toHaveStyle("text-decoration: line-through;");
+  });
 
-  // it("does not show line-through on span when done is false", () => {
-  //   const { span } = setup({ todo: { ...sampleTodo, done: false } });
-  //   expect(span).not.toHaveStyle("text-decoration: line-through;");
-  // });
+  it("does not show line-through on span when done is false", () => {
+    const { span } = setup({ todo: { ...sampleTodo, done: false } });
+    expect(span).not.toHaveStyle("text-decoration: line-through;");
+  });
 });
