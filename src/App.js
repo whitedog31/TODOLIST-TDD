ecf6597320f2529788f35a6d6e92a273ef3241bf
김민수 <jasonkim@mondrian.ai>
2022-06-09 12:02:11 +0900

    feat:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import TodoForm from "./TodoForm";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <TodoForm />
+    </div>
+  );
 }
